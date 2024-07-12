1b7d750a73d853ed7d15cc9a5d3dd657610a06f6
= <=>
2024-07-12 17:38:42 +0300

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {// tersten de aynı olan kelimeler ana,adda,kabak,...
+        Scanner input = new Scanner(System.in);
+        System.out.print("Word: ");
+        String word = input.nextLine();
+        System.out.println(isPolindromic(word));
+    }
+
+
+    static boolean isPolindromic(String word) {
+        int length = word.length();
+        boolean isPolindrom = true;
+        for (int i = 0; i <= length / 2; i++) {
+            if (word.charAt(i) != word.charAt((length - 1) - i)) {
+                isPolindrom = false;
+                break;
+            }
+        }
+        return isPolindrom;
     }
 }
